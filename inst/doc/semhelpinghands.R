710f9217986ccb1b9cc1ce399d40906602496114
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-09-17 13:10:02 +0000

    version 0.1.9

--- inst/doc/semhelpinghands.R
+++ inst/doc/semhelpinghands.R
@@ -1,4 +1,4 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
@@ -16,7 +16,8 @@ y1 ~ x1 + x2 + x3
 y2 ~ x1 + x3
 y3 ~ y2 + x2
 "
-fit <- sem(model = mod, data = dvs_ivs,
+fit <- sem(model = mod,
+           data = dvs_ivs,
            fixed.x = FALSE)
 
 ## -----------------------------------------------------------------------------
@@ -24,7 +25,8 @@ est <- parameterEstimates(fit)
 est
 
 ## -----------------------------------------------------------------------------
-fit_gp <- sem(model = mod, data = dvs_ivs,
+fit_gp <- sem(model = mod,
+              data = dvs_ivs,
               group = "gp",
               fixed.x = FALSE)
 
@@ -36,25 +38,32 @@ est_gp
 add_sig(est)
 
 ## -----------------------------------------------------------------------------
-add_sig(est, use = c("pvalue", "ci"))
+add_sig(est,
+        use = c("pvalue", "ci"))
 
 ## -----------------------------------------------------------------------------
 std <- standardizedSolution(fit)
 add_sig(std)
 
 ## -----------------------------------------------------------------------------
-filter_by(est, op = "~")
+filter_by(est,
+          op = "~")
 
 ## -----------------------------------------------------------------------------
-filter_by(est_gp, op = "~", group = "gp1", fit = fit_gp)
+filter_by(est_gp,
+          op = "~",
+          group = "gp1",
+          fit = fit_gp)
 
 ## -----------------------------------------------------------------------------
 group_by_dvs(est)
 group_by_ivs(est)
 
 ## -----------------------------------------------------------------------------
-group_by_dvs(est, col_name = "pvalue")
-group_by_ivs(est, col_name = "pvalue")
+group_by_dvs(est,
+             col_name = "pvalue")
+group_by_ivs(est,
+             col_name = "pvalue")
 
 ## -----------------------------------------------------------------------------
 group_by_groups(est_gp)
@@ -74,8 +83,9 @@ y1 ~ x1 + x2 + x3
 y2 ~ x1 + x2
 y3 ~ y2 + x1
 "
-fit2 <- sem(model = mod2, data = dvs_ivs,
-           fixed.x = FALSE)
+fit2 <- sem(model = mod2,
+            data = dvs_ivs,
+            fixed.x = FALSE)
 est2 <- parameterEstimates(fit2)
 est2
 
@@ -91,8 +101,10 @@ group_by_models(list(Model1 = est,
 ## -----------------------------------------------------------------------------
 out <- group_by_groups(est_gp,
                        col_names = c("est", "pvalue"))
-out <- filter_by(out, op = c("~", "~~"))
-sort_by(out, by = c("op", "rhs"))
+out <- filter_by(out,
+                 op = c("~", "~~"))
+sort_by(out,
+        by = c("op", "rhs"))
 
 ## -----------------------------------------------------------------------------
 est_gp |>
@@ -104,10 +116,12 @@ est_gp |>
 ## -----------------------------------------------------------------------------
 out <- compare_estimators(fit,
          estimator = c("ML", "GLS", "MLR"))
-group_by_models(out, col_names = c("se", "pvalue"))
+group_by_models(out,
+                col_names = c("se", "pvalue"))
 
 ## -----------------------------------------------------------------------------
-se_ratios(out, reference = "ML")
+se_ratios(out,
+          reference = "ML")
 
 ## -----------------------------------------------------------------------------
 data(dvs_ivs)
@@ -117,11 +131,16 @@ y1 ~ x1 + x2 + x3
 y2 ~ x1 + x3
 y3 ~ y2 + x2
 "
-fit_default <- sem(model = mod, data = dvs_ivs)
+fit_default <- sem(model = mod,
+                   data = dvs_ivs)
 show_more_options(fit_default)
-fit_MLR <- sem(model = mod, data = dvs_ivs, estimator = "MLR")
+fit_MLR <- sem(model = mod,
+               data = dvs_ivs,
+               estimator = "MLR")
 show_more_options(fit_MLR)
-fit_MLR_fiml <- sem(model = mod, data = dvs_ivs, estimator = "MLR",
-               missing = "fiml")
+fit_MLR_fiml <- sem(model = mod,
+                    data = dvs_ivs,
+                    estimator = "MLR",
+                    missing = "fiml")
 show_more_options(fit_MLR_fiml)
 
